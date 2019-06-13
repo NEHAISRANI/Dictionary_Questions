9c7744f5ee2726a50a97667107ba325fe4de6853
NEHAISRANI <nehai18@navgurukul.org>
2019-06-13 12:34:19 +0530

    in this program multiplicaion of dictionary values

--- sum_dictionary_value.py
+++ sum_dictionary_value.py
@@ -1,10 +1,10 @@
 dict={"neha":5,"prgati":10,"priya":6,"sonam":7}
 value= dict.values()#dictionary me jitni bhi key ki values hai print ho jayegi 
+print"multiplication of dictionary value"
 i=0
-sum=0
-print"sum of dictionary value"
-while i<len(value):
-    sum=sum+value[i]
+mul=1
+while i<len(value):#dictionary ki value ki length jitni hai wahi tak chalega
+    mul=value[i]*mul
     i=i+1
-print sum  # dictionary ki values ka sum print hoga
+print mul # dictionary ki values ka multiplication print hoga
 
