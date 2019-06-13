206a1fc584908220815e1592fbd30fa7e5a84300
NEHAISRANI <nehai18@navgurukul.org>
2019-06-13 13:12:53 +0530

    in this code multiplicaion dictionary value

---
+++ mul_dictionary_value.py
@@ -0,0 +1,9 @@
+dict={"neha":5,"prgati":10,"priya":6,"sonam":7}
+value= dict.values()#dictionary me jitni bhi key ki values hai print ho jayegi 
+print"multiplication of dictionary value"
+i=0
+mul=1
+while i<len(value):#dictionary ki value ki length jitni hai wahi tak chalega
+    mul=value[i]*mul
+    i=i+1
+print mul # dictionary ki values ka multiplication print hoga
