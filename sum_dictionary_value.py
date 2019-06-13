6dec4093b74ef074e99a33ae6961c1c1f64f3b68
NEHAISRANI <nehai18@navgurukul.org>
2019-06-13 11:57:44 +0530

    first commit

---
+++ sum_dictionary_value.py
@@ -0,0 +1,10 @@
+dict={"neha":5,"prgati":10,"priya":6,"sonam":7}
+value= dict.values()#dictionary me jitni bhi key ki values hai print ho jayegi 
+i=0
+sum=0
+print"sum of dictionary value"
+while i<len(value):
+    sum=sum+value[i]
+    i=i+1
+print sum  # dictionary ki values ka sum print hoga
+
